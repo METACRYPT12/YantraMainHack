18631a868c7a6c134b7d1f021b2250ef16168a33
Chaitanya Tejaswi <chaitanya.tejasvi@gmail.com>
2024-04-13 05:37:42 +0530

    Login backend

---
+++ client/src/components/home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = (props) => {
+  const { loggedIn, email } = props;
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    if (loggedIn) {
+      localStorage.removeItem("user");
+      props.setLoggedIn(false);
+    } else {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <div className="mainContainer">
+      <div className="titleContainer">Welcome!</div>
+      <div>Home page</div>
+      <div className={"ButtonContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? "Log out" : "Log in"}
+        />
+        {loggedIn ? <div>You email is: {email}</div> : <div />}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
